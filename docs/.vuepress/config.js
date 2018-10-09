065b4870503226dd61840b644802cd31cecb6f3a
zhanyunchencc <415205516@qq.com>
2018-10-09 23:26:08 +0800

    init vuepress blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: '个人主页',
+  description: 'Personal Website',
+  head: [ // 注入到当前页面的 HTML <head> 中的标签
+    ['link', { rel: 'icon', href: '/photo.jpg' }],
+    ['link', { rel: 'manifest', href: '/photo.jpg' }],
+    ['link', { rel: 'apple-touch-icon', href: '/photo.jpg' }],
+  ],
+  serviceWorker: true, // 是否开启 PWA
+  base: '/', // 部署到github相关的配置
+  markdown: {
+    lineNumbers: true // 代码块是否显示行号
+  },
+  themeConfig: {
+    nav:[ // 导航栏配置
+      {text: '前端基础', link: '/accumulate/' },
+      {text: '算法题库', link: '/algorithm/'},
+      {text: '诗和远方', link: '/others/'},
+      {text: '微博', link: 'https://baidu.com'}      
+    ],
+    sidebar: 'auto', // 侧边栏配置
+    sidebarDepth: 2
+  }
+};
